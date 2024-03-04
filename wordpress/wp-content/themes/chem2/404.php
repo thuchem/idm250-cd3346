0475173e7c07dc97ac2ea23f36086fc9974fca3e
thuchem <102186268+thuchem@users.noreply.github.com>
2024-03-04 02:53:14 -0500

    Update 404.php

--- wordpress/wp-content/themes/chem2/404.php
+++ wordpress/wp-content/themes/chem2/404.php
@@ -1,2 +1,17 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ */
 
-<h1>404 Pageee</h1>
+get_header(); ?>
+
+  <body>
+    <div class="element-php">
+      <div class="div">
+        <div class="text-wrapper">:(</div>
+        <div class="text-wrapper-2">404.</div>
+      </div>
+    </div>
+  </body>
+</html>
