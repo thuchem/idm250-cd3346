59e2721e4645833edee32b83a348a62fae403588
thuchem <102186268+thuchem@users.noreply.github.com>
2024-03-04 02:37:09 -0500

    Update and rename single-products.php to single-projects.php

---
+++ wordpress/wp-content/themes/chem2/single-projects.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<!-- <h1>
+  <?php echo get_the_title(); ?>
+</h1> -->
+<div class="content-page">
+  <?php
+    echo get_the_content();
+  ?>
+</div>
+<!-- <p>This is a single-projects.php template</p> -->
+<?php get_footer(); ?>
